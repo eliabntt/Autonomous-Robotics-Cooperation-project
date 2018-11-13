dcebf014af4c9f71260da30ff4e6208141c5c480
Matteo Dal Zovo <zovomat@gmail.com>
2018-11-14 00:01:34 +0100

    args and tags crossing

--- src/discover.cpp
+++ src/discover.cpp
@@ -5,6 +5,26 @@
 #include <geometry_msgs/PoseArray.h>
 #include <apriltags_ros/AprilTagDetectionArray.h>
 
+std::vector<std::string> params;
+std::vector<std::string> tagnames = {
+        "red_cube_1",
+        "red_cube_2",
+        "red_cube_3",
+        "red_cube_4",
+        "yellow_cyl_1",
+        "yellow_cyl_2",
+        "green_triangle_1",
+        "green_triangle_2",
+        "green_triangle_3",
+        "blue_cube_1",
+        "blue_cube_2",
+        "blue_cube_3",
+        "blue_cube_4",
+        "red_triangle_1",
+        "red_triangle_2",
+        "red_triangle_3"
+};
+
 void detectionsPoseCallback(const geometry_msgs::PoseArray::ConstPtr& input){
     // todo: save to file instead of this
     if (!input->poses.empty())
@@ -24,21 +44,26 @@ void detectionsPoseCallback(const geometry_msgs::PoseArray::ConstPtr& input){
 void detectionsCallback(const apriltags_ros::AprilTagDetectionArray::ConstPtr& input){
     // todo: save to file instead of this
     for(apriltags_ros::AprilTagDetection tag : input->detections) {
-                    std::vector<double> temp = std::vector<double>();
-                    double idt = tag.id;
-                    temp.push_back(idt);
-                    temp.push_back(tag.size);
-                    temp.push_back(tag.pose.pose.orientation.x);
-                    temp.push_back(tag.pose.pose.orientation.y);
-                    temp.push_back(tag.pose.pose.orientation.z);
-                    temp.push_back(tag.pose.pose.orientation.w);
-                    temp.push_back(tag.pose.pose.position.x);
-                    temp.push_back(tag.pose.pose.position.y);
-                    temp.push_back(tag.pose.pose.position.z);
-                }
+        std::vector<double> temp = std::vector<double>();
+        double idt = tag.id;
+        if(std::find(params.begin(), params.end(), tagnames[int(idt)]) != params.end()) {
+            temp.push_back(idt);
+            temp.push_back(tag.size);
+            temp.push_back(tag.pose.pose.orientation.x);
+            temp.push_back(tag.pose.pose.orientation.y);
+            temp.push_back(tag.pose.pose.orientation.z);
+            temp.push_back(tag.pose.pose.orientation.w);
+            temp.push_back(tag.pose.pose.position.x);
+            temp.push_back(tag.pose.pose.position.y);
+            temp.push_back(tag.pose.pose.position.z);
+        }
+    }
 }
 
-int main(int argc, char **argv){
+int main(int argc, char *argv[]){
+
+    std::vector<std::string> args_vector(argv, argv + argc);
+    params = args_vector;
     ros::init(argc, argv, "discover");
     ros::NodeHandle n;
     ros::Subscriber sub_comprehensive = n.subscribe<apriltags_ros::AprilTagDetectionArray>("/tag_detections", 1, detectionsCallback);
