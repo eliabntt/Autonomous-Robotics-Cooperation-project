4ae03a21be685cc26b3b89263a5bd633408f1014
eliabntt <eliabntt94@gmail.com>
2018-11-14 09:55:22 +0100

    better vectors initialization

--- hw1_perception/src/discover.cpp
+++ hw1_perception/src/discover.cpp
@@ -25,28 +25,29 @@ std::vector<std::string> tagnames = {
         "red_triangle_3"
 };
 
-void detectionsPoseCallback(const geometry_msgs::PoseArray::ConstPtr& input){
+void detectionsPoseCallback(const geometry_msgs::PoseArray::ConstPtr &input) {
     // todo: save to file instead of this
-    if (!input->poses.empty())
-    {
-      geometry_msgs::Pose output;
-      output.position.x = input->poses[0].position.x;
-      output.position.y = input->poses[0].position.y;
-      output.position.z = input->poses[0].position.z;
-      output.orientation.x = input->poses[0].orientation.x;
-      output.orientation.y = input->poses[0].orientation.y;
-      output.orientation.z = input->poses[0].orientation.z;
-      output.orientation.w = input->poses[0].orientation.w;
+    if (!input->poses.empty()) {
+        geometry_msgs::Pose output;
+        output.position.x = input->poses[0].position.x;
+        output.position.y = input->poses[0].position.y;
+        output.position.z = input->poses[0].position.z;
+        output.orientation.x = input->poses[0].orientation.x;
+        output.orientation.y = input->poses[0].orientation.y;
+        output.orientation.z = input->poses[0].orientation.z;
+        output.orientation.w = input->poses[0].orientation.w;
     }
 }
 
 
-void detectionsCallback(const apriltags_ros::AprilTagDetectionArray::ConstPtr& input){
+void detectionsCallback(const apriltags_ros::AprilTagDetectionArray::ConstPtr &input) {
     // todo: save to file instead of this
-    for(apriltags_ros::AprilTagDetection tag : input->detections) {
+    ROS_INFO_STREAM("CACCA");
+    for (apriltags_ros::AprilTagDetection tag : input->detections) {
         std::vector<double> temp = std::vector<double>();
         double idt = tag.id;
-        if(std::find(params.begin(), params.end(), tagnames[int(idt)]) != params.end()) {
+        if (std::find(params.begin(), params.end(), tagnames[int(idt)]) != params.end()) {
+            ROS_INFO_STREAM(idt);
             temp.push_back(idt);
             temp.push_back(tag.size);
             temp.push_back(tag.pose.pose.orientation.x);
@@ -60,16 +61,25 @@ void detectionsCallback(const apriltags_ros::AprilTagDetectionArray::ConstPtr& i
     }
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-    std::vector<std::string> args_vector(argv, argv + argc);
-    params = args_vector;
     ros::init(argc, argv, "discover");
+
+    if (argc == 1)
+        params = tagnames;
+    else {
+        for (int i = 1; i < argc; i++) {
+            if (std::find(tagnames.begin(), tagnames.end(), argv[i]) != tagnames.end())
+                params.emplace_back(argv[i]);
+            else
+                ROS_INFO_STREAM(argv[i] << " is NOT a valid tag");
+        }
+    }
+
     ros::NodeHandle n;
-    ros::Subscriber sub_comprehensive = n.subscribe<apriltags_ros::AprilTagDetectionArray>("/tag_detections", 1, detectionsCallback);
-
-    ros::Subscriber sub_only_pose = n.subscribe<geometry_msgs::PoseArray>("/tag_detections_pose", 1, detectionsPoseCallback);
-
+    ros::Subscriber sub_comprehensive = n.subscribe<apriltags_ros::AprilTagDetectionArray>("/tag_detections", 1,
+                                                                                           detectionsCallback);
+//todo see why spinonce() not working
     ros::spin();
     return 0;
 }
