847be0bb5954a86ad589980535ee7a6b72f3c1a4
Filippo Rigotto <rigotto.filippo@gmail.com>
2018-11-15 18:27:14 +0100

    Included forever keyword detection in argv Removed useless code and files Output file formatting Some comments and TODO

--- hw1_perception/src/discover.cpp
+++ hw1_perception/src/discover.cpp
@@ -1,15 +1,14 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "ros/package.h"
-
 #include <fstream>
 
-#include <geometry_msgs/Pose.h>
-#include <geometry_msgs/PoseArray.h>
-#include <apriltags_ros/AprilTagDetectionArray.h>
+#include "ros/ros.h"
+#include "ros/package.h"
+#include "std_msgs/String.h" // todo maybe unused
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PoseArray.h"
+#include "apriltags_ros/AprilTagDetectionArray.h"
 
-std::vector<std::string> params;
-const std::vector<std::string> tagnames = {
+std::vector <std::string> params;
+const std::vector <std::string> tagnames = {
         "red_cube_1",
         "red_cube_2",
         "red_cube_3",
@@ -28,71 +27,67 @@ const std::vector<std::string> tagnames = {
         "red_triangle_3"
 };
 bool stop = false;
-
-void detectionsPoseCallback(const geometry_msgs::PoseArray::ConstPtr &input) {
-    // todo: save to file instead of this
-    if (!input->poses.empty()) {
-        geometry_msgs::Pose output;
-        output.position.x = input->poses[0].position.x;
-        output.position.y = input->poses[0].position.y;
-        output.position.z = input->poses[0].position.z;
-        output.orientation.x = input->poses[0].orientation.x;
-        output.orientation.y = input->poses[0].orientation.y;
-        output.orientation.z = input->poses[0].orientation.z;
-        output.orientation.w = input->poses[0].orientation.w;
-    }
-}
-
+bool forever = false;
 
 void detectionsCallback(const apriltags_ros::AprilTagDetectionArray::ConstPtr &input) {
-    std::string file_path = ros::package::getPath("hw1_perception")+"/output.txt";
+    // set output file name and open a stream on it
+    std::string file_path = ros::package::getPath("hw1_perception") + "/output.txt";
     std::fstream output_file(file_path, std::fstream::out);
-    if (!(output_file.is_open())){
-        ROS_INFO_STREAM("error: failed opening file, data won't be saved");
-    }
-    output_file << "detected frames:\n";
+    if (!(output_file.is_open()))
+        ROS_INFO_STREAM("ERROR: failure opening file, data won't be saved");
+
+    output_file << "Detected frames, w.r.t. Kinect reference frame:\n";
     for (apriltags_ros::AprilTagDetection tag : input->detections) {
-        std::vector<double> temp = std::vector<double>();
-        int idt = int(tag.id);
+        int idt = (int) tag.id;
         if (std::find(params.begin(), params.end(), tagnames[idt]) != params.end()) {
+            // tag found over objects on the table
             ROS_INFO_STREAM(idt);
-            output_file << "tag " << idt << " name: " << tagnames[idt] << "\n";
-            output_file << "    size: " << tag.size << "\n";
-            output_file << "    orientation: x=" << tag.pose.pose.orientation.x
-                << " y= " << tag.pose.pose.orientation.y
-                << " z= " << tag.pose.pose.orientation.z
-                << " w= " << tag.pose.pose.orientation.w << "\n";
-            output_file << "    position: x=" << tag.pose.pose.position.x
-                << " y= " << tag.pose.pose.position.y
-                << " z= " << tag.pose.pose.position.z << "\n";
+
+            output_file << "tag id: " << idt << std::endl
+                        << "frame id: " << tagnames[idt] << std::endl;
+            output_file << "    size: " << tag.size << std::endl; // todo FR is this value important?
+            output_file << "    orientation: x = " << tag.pose.pose.orientation.x
+                        << "  y = " << tag.pose.pose.orientation.y
+                        << "  z = " << tag.pose.pose.orientation.z
+                        << "  w = " << tag.pose.pose.orientation.w << std::endl;
+            output_file << "    position: x = " << tag.pose.pose.position.x
+                        << "  y = " << tag.pose.pose.position.y
+                        << "  z = " << tag.pose.pose.position.z << std::endl << std::endl;
         }
     }
     output_file.close();
-    stop = true;
+    stop = true; // useless in forever mode
 }
 
 int main(int argc, char *argv[]) {
-
     ros::init(argc, argv, "discover");
 
-
-    if (argc == 1)
+    if (argc == 1) {
+        ROS_INFO_STREAM("No parameters passed. Detecting all possible tags");
         params = tagnames;
-    else {
-        for (int i = 1; i < argc; i++) {
+    } else {
+        for (int i = 1; i < argc; i++)
+            // for each parameter, check if it is a frame id and save it in a list
             if (std::find(tagnames.begin(), tagnames.end(), argv[i]) != tagnames.end())
-                params.emplace_back(argv[i]);
-            else
-                ROS_INFO_STREAM(argv[i] << " is NOT a valid tag");
-        }
+                params.push_back(argv[i]);
+            else if (argv[i] == std::string("forever")) {
+                ROS_INFO_STREAM("Forever parameter detected. Scan will not end.");
+                forever = true;
+            } else
+                ROS_INFO_STREAM(argv[i] << " is NOT a valid tag or keyword");
     }
 
     ros::NodeHandle n;
-    ros::Subscriber sub_comprehensive = n.subscribe<apriltags_ros::AprilTagDetectionArray>("/tag_detections", 1,
-                                                                                           detectionsCallback);
+    ros::Subscriber sub = n.subscribe<apriltags_ros::AprilTagDetectionArray>("/tag_detections", 1, detectionsCallback);
 
-    while (ros::ok() && !stop) {
-        ros::spinOnce();
+    if (forever)
+        ros::spin();
+    else {
+        // a single spinOnce call won't work, so repeating the call
+        // until first detection message arrives, then stop
+        while (ros::ok() && !stop)
+            ros::spinOnce();
     }
+
     return 0;
 }
