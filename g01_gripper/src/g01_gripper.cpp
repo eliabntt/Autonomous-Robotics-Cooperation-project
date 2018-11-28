3de86f9e197dedb4607afad24f0ea5184dc3c953
eliabntt <eliabntt94@gmail.com>
2018-11-28 14:29:00 +0100

    open/close working

--- g01_gripper/src/g01_gripper.cpp
+++ g01_gripper/src/g01_gripper.cpp
@@ -6,46 +6,96 @@
 
 #include "g01_gripper.h"
 
+G01Gripper::G01Gripper() : command(), n(){
 
-G01Gripper::G01Gripper() {
-    ros::NodeHandle n;
+    //init
+    //todo implement better for wrong inputs(see hw1)
+    sim = n.hasParam("sim") ? n.getParam("sim", sim) : true;
 
-    ros::AsyncSpinner spinner(1);
+    ros::AsyncSpinner spinner(2);
     spinner.start();
 
     //robot
     std::string PLANNING_GROUP = "manipulator";
-    moveit::planning_interface::MoveGroupInterface move_group("manipulator");
+    moveit::planning_interface::MoveGroupInterface my_group(PLANNING_GROUP);
 
     //scene
     moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+    //todo mmmm
     const robot_state::JointModelGroup* joint_model_group =
-        move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-
-    ROS_INFO_NAMED("tutorial", "Planning frame: %s", move_group.getPlanningFrame().c_str());
+        my_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
 
-    geometry_msgs::Pose target_pose1;
-    target_pose1.orientation.x = 0.001;
-    target_pose1.orientation.y = 0.007107;
-    target_pose1.position.x = 0.0037766;
-    target_pose1.position.y = -0.0043118;
-    target_pose1.position.z = -0.00549799;
-    move_group.setPoseTarget(target_pose1);
+    //basic info
+    ROS_INFO("Reference frame: %s", my_group.getPlanningFrame().c_str());
+    ROS_INFO("Reference frame: %s", my_group.getEndEffectorLink().c_str());
 
-    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-    moveit::planning_interface::MoveItErrorCode success = move_group.plan(my_plan);
+    gripperCommandPub = n.advertise<robotiq_s_model_control::SModel_robot_output>("/robotiq_hands/l_hand/SModelRobotOutput",1);
 
+    close(255);
 
-    ROS_INFO_STREAM("tutorial" << "Visualizing plan 1 (pose goal) %s" << (success == moveit_msgs::MoveItErrorCodes::SUCCESS ? "" :
-                                                                    "FAILED"));
-    move_group.move();
+/*
+    if(sim)
+        my_group.detachObject(id);
+
+    if(sim)
+        my_group.attatchObject(id, my_group.getEndEffectorLink().c_str());
+ */
 }
 
-bool G01Gripper::move(double x, double y) {
-    return false;
+void G01Gripper::open() {
+    command.rACT = 1;
+    command.rMOD= 0;
+    command.rGTO= 1;
+    command.rATR= 0;
+    command.rGLV= 0;
+    command.rICF= 0;
+    command.rICS= 0;
+    command.rPRA= 0;
+    command.rSPA= 0;
+    command.rFRA= 0;
+    command.rPRB= 0;
+    command.rSPB= 0;
+    command.rFRB= 255;
+    command.rPRC= 0;
+    command.rSPC= 0;
+    command.rFRC= 0;
+    command.rPRS= 0;
+    command.rSPS= 0;
+    command.rFRS= 0;
+    gripperCommandPub.publish(command);
 }
 
-
-
-
+void G01Gripper::close(int howMuch) {
+    assert(howMuch > 0);
+    //if is active
+    //todo complete
+    if(command.rACT == 1)
+    {
+        ROS_INFO_STREAM("a");
+        return;
+    }
+    command.rACT = 1;
+    command.rMOD= 0;
+    command.rGTO= 1;
+    command.rATR= 0;
+    command.rGLV= 0;
+    command.rICF= 0;
+    command.rICS= 0;
+    command.rPRA= 255;
+    command.rSPA= 255;
+    command.rFRA= 150;
+    command.rPRB= 0;
+    command.rSPB= 0;
+    command.rFRB= 0;
+    command.rPRC= 0;
+    command.rSPC= 0;
+    command.rFRC= 0;
+    command.rPRS= 0;
+    command.rSPS= 0;
+    command.rFRS= 0;
+    gripperCommandPub.publish(command);
+    ROS_INFO_STREAM("prova");
+    return;
+}
