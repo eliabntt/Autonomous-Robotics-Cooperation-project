44645597edc4704cda14f44d6b75f3c0598ef306
Elia Bonetto <eliabntt94@gmail.com>
2018-11-28 10:03:34 +0100

    initial configuration with class

---
+++ g01_gripper/include/g01_gripper.h
@@ -0,0 +1,25 @@
+//
+// Created by eliabntt on 28/11/18.
+//
+#include <math.h>
+#include <string>
+
+#include <ros/ros.h>
+#include <std_msgs/Bool.h>
+
+
+
+#ifndef G01_GRIPPER_G01_GRIPPER_H
+#define G01_GRIPPER_G01_GRIPPER_H
+
+class G01Gripper{
+public:
+    //Default constructor
+    G01Gripper();
+
+
+private:
+
+};
+
+#endif
---
+++ g01_gripper/src/g01_gripper.cpp
@@ -0,0 +1,14 @@
+//
+// Created by eliabntt on 28/11/18.
+//
+
+#include "../include/g01_gripper.h"
+
+G01Gripper::G01Gripper() {
+
+
+}
+
+
+
+
