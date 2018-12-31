ce01c00d01f3d70cf10efb30355c9f9676e9bbf8
eliabntt <eliabntt94@gmail.com>
2018-12-31 14:19:17 +0100

    some minors

--- g01_move/include/g01_move.h
+++ g01_move/include/g01_move.h
@@ -16,6 +16,29 @@ private:
     bool sim;
     ros::NodeHandle n;
 
+    //fixme
+    /*
+    loading_pose
+    unloading_pose
+    start_corridor_pose
+    end_corridor_pose
+    */
+
+    /*
+    REPEAT
+    1 -> start corridor
+     a. -> start wall following https://syrotek.felk.cvut.cz/course/ROS_CPP_INTRO/exercise/ROS_CPP_WALLFOLLOWING
+                                https://github.com/ssscassio/ros-wall-follower-2-wheeled-robot
+            since I
+            have only to go straight and to check the distance from the front wall
+    2 -> loading
+     a. -> get distance from end wall and side wall
+     b. -> publish for LZ branch
+    3. -> end corridor(rotate and position the robot)
+    4. -> start corrdior
+    5. -> unloading pose
+    */
+
 };
 
 #endif
