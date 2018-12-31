91dace8cf5f3a993d59f7605f3c621cb19bd57db
eliabntt <eliabntt94@gmail.com>
2018-12-31 12:51:31 +0100

    seems to do something at least

--- g01_move/src/move.cpp
+++ g01_move/src/move.cpp
@@ -1,11 +1,45 @@
 #include <fstream>
-#include "g01_move.h"
 
-int main(int argc, char *argv[]) {
-    ros::init(argc, argv, "g01_move");
+//
+// Created by rig8f on 26/12/18.
+//
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
 
-    G01Move m = G01Move();
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-    ros::waitForShutdown();
+int main(int argc, char** argv){
+    ros::init(argc, argv, "move");
+
+    ROS_INFO_STREAM("CACCA");
+    //tell the action client that we want to spin a thread by default
+    MoveBaseClient ac("marrtino/move_base", true);
+    //wait for the action server to come up
+    while(!ac.waitForServer(ros::Duration(5.0))){
+        ROS_INFO("Waiting for the move_base action server to come up");
+    }
+
+    move_base_msgs::MoveBaseGoal goal;
+
+    //we'll send a goal to the robot to move 1 meter forward
+    goal.target_pose.header.frame_id = "world";
+    goal.target_pose.header.stamp = ros::Time::now();
+
+    goal.target_pose.pose.position.x =  -1;
+    goal.target_pose.pose.position.y =  1;
+    goal.target_pose.pose.orientation.w = 1.0;
+
+    ROS_INFO("Sending goal");
+    ac.sendGoal(goal);
+
+    ac.waitForResult();
+
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+        ROS_INFO("Hooray, the base moved 1 meter forward");
+    else
+        ROS_INFO("The base failed to move forward 1 meter for some reason");
+
     return 0;
 }
+
