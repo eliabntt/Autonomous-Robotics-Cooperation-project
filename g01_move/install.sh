64f32e8617c1b731e8afcb0e1e20fc4e2974f256
eliabntt <eliabntt94@gmail.com>
2019-01-06 13:08:57 +0100

    better install

--- g01_move/install.sh
+++ g01_move/install.sh
@@ -5,10 +5,10 @@ mkdir ../back/config
 #backup current config
 cp * ../back/config
 #overwrite with ours
-cp ../../../g01_move/conf/yaml ./
+cp ../../../g01_move/conf/yaml/* ./
 #backup move_base.launch
 cd ../launch
 mkdir ../back/launch
-cp move_base.launch ../back/launch
+cp ./move_base.launch ../back/launch
 #overwrite
 cp ../../../g01_move/conf/launch/move_base.launch ./
