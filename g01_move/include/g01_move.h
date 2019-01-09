d62f4fe02d249b4a9d943f16b6bed27ada557dfe
eliabntt <eliabntt94@gmail.com>
2019-01-09 15:25:49 +0100

    New features: 	dynamic padding New initial and entrance pose Deleted no longer useful method for centering Deleted useless poseToYPR

--- g01_move/include/g01_move.h
+++ g01_move/include/g01_move.h
@@ -13,6 +13,14 @@
 #include <sensor_msgs/LaserScan.h>
 #include <std_srvs/Empty.h>
 
+
+
+#include <dynamic_reconfigure/DoubleParameter.h>
+#include <dynamic_reconfigure/Reconfigure.h>
+#include <dynamic_reconfigure/Config.h>
+
+
+
 #ifndef G01_MOVE_G01_MOVE_H
 #define G01_MOVE_G01_MOVE_H
 
@@ -23,6 +31,9 @@ public:
     G01Move();
 
 private:
+
+    void changePadding(bool increase);
+
     // MEMBERS
     bool sim;
     ros::NodeHandle n;
@@ -40,7 +51,7 @@ private:
     void subPoseOdomCallback(const nav_msgs::Odometry::ConstPtr& msgOdom);
 
     // plan-based moving methods
-    bool moveToGoal(move_base_msgs::MoveBaseGoal goal, bool inside = false);
+    bool moveToGoal(move_base_msgs::MoveBaseGoal goal);
     bool inPlaceCW90(move_base_msgs::MoveBaseGoal &pos);
     bool inPlaceCCW90(move_base_msgs::MoveBaseGoal &pos);
 
@@ -59,42 +70,13 @@ private:
     geometry_msgs::Twist moveCommand;
     bool isManualModeDone = false;
 
-    void recoverManual(bool inside, bool rot = false);
+    void recoverManual(bool rot = false);
     void wallFollower(bool forward);
     void forwardCallback(const sensor_msgs::LaserScan::ConstPtr &msg);
     void backwardCallback(const sensor_msgs::LaserScan::ConstPtr &msg);
     void averageLR(const sensor_msgs::LaserScan::ConstPtr &msg);
 
-    void poseToYPR(geometry_msgs::Pose pose, double *yaw, double *pitch, double *roll);
 
-    /* fixme useful resources
-    http://www.theconstructsim.com/ros-projects-exploring-ros-using-2-wheeled-robot-part-1/
-    http://www.theconstructsim.com/ros-qa-126-how-to-configure-the-differential-drive-ros-controller/
-    https://answers.ros.org/question/289561/help-to-run-diff_drive_controller/
-    https://books.google.it/books?id=skFPDwAAQBAJ&pg=PA182&lpg=PA182&dq=two+wheeled+robot+ros+yaml+config&source=bl&ots=fgpCIFfQSa&sig=90Tbzlv0YMDtm_aLxqukdQby_Q4&hl=en&sa=X&ved=2ahUKEwiYh4CXocrfAhXF-aQKHTjqBvIQ6AEwBXoECAUQAQ#v=onepage&q=two%20wheeled%20robot%20ros%20yaml%20config&f=false
-
-    https://www.youtube.com/watch?v=q3Dn5U3cSWk
-
-    http://wiki.ros.org/navigation/Tutorials/RobotSetup#Costmap_Configuration_.28local_costmap.29_.26_.28global_costmap.29
-    https://answers.ros.org/question/279148/costmap-parameters-navigation-stack/
-    https://answers.ros.org/question/219108/ghost-obstacles-on-local-costmap/
-    https://answers.ros.org/question/277769/how-to-decrease-obstacle-area-in-costmaps/
-
-    https://answers.ros.org/question/268485/navigation-planning-too-near-to-obstacles/
-    https://answers.ros.org/question/12874/costmap2d-inflation-radius/
-
-    REPEAT
-    1 -> start corridor
-     a. -> start wall following https://syrotek.felk.cvut.cz/course/ROS_CPP_INTRO/exercise/ROS_CPP_WALLFOLLOWING
-                                https://github.com/ssscassio/ros-wall-follower-2-wheeled-robot
-            since I have only to go straight and to check the distance from the front wall
-    2 -> loading
-     a. -> get distance from end wall and side wall
-     b. -> publish for LZ branch
-    3. -> end corridor(rotate and position the robot)
-    4. -> start corrdior
-    5. -> unloading pose
-    */
 };
 
 #endif
