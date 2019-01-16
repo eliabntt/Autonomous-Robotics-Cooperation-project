7a7810de258f881e5e47dd3689acc1c3feda3e7b
eliabntt <eliabntt94@gmail.com>
2019-01-16 10:01:42 +0100

    removed useless scripts

--- g01_move/install.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash install.sh
-cd ../marrtino/marrtino_navigation/
-mkdir ./back
-cp config/ back/ -r
-cp launch/ back/ -r
-cp maps/ back/ -r
-
-#overwrite with ours
-cp ../../g01_move/conf/* ./ -r
