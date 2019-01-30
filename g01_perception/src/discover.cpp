8909e42e67a84a4fc8456ca1fff268550a9c043a
Filippo Rigotto <rigotto.filippo@gmail.com>
2019-01-30 21:38:14 +0100

    Correct header files install and referencing.

--- g01_perception/src/discover.cpp
+++ g01_perception/src/discover.cpp
@@ -10,7 +10,7 @@
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "tf2_ros/transform_listener.h"
 #include "g01_perception/PoseStampedArray.h"
-#include "../include/tags.h"
+#include "g01_perception/tags.h"
 
 std::vector<std::string> params;
 
